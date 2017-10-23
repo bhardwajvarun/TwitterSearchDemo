90191a2021e7e52da7474b331d2dc0bad3557d5b
Varun Bhardwaj <varunbhardwaj@me.com>
2017-10-24 00:58:44 +0530

    update spec cases

--- client/app/home/home.component.spec.ts
+++ client/app/home/home.component.spec.ts
@@ -1,7 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { APP_BASE_HREF } from '@angular/common';
 
-import { HomeComponent } from './home.component';
+import { SharedModule } from '../shared/shared.module';
+import { RoutingModule } from '../routing.module';
+
+import { HomeComponent } from '../home/home.component';
+import { SearchService } from '../services/search.service';
+
+import { TweetComponent } from '../tweet/tweet.component';
+import { TweetService } from '../services/tweet.service';
+
 
 describe('Component: Home', () => {
   let component: HomeComponent;
@@ -9,7 +18,16 @@ describe('Component: Home', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      imports: [ 
+        SharedModule,
+        RoutingModule
+      ],      
+      declarations: [ HomeComponent , TweetComponent],
+      providers: [
+        TweetService,
+        SearchService,
+        {provide: APP_BASE_HREF, useValue: '/'}
+      ]
     })
     .compileComponents();
   }));
--- client/app/tweet/tweet.component.spec.ts
+++ client/app/tweet/tweet.component.spec.ts
@@ -1,2 +1,45 @@
-import { TestBed, async } from '@angular/core/testing';
-import { TweetComponent } from './tweet.component';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { APP_BASE_HREF } from '@angular/common';
+
+import { SharedModule } from '../shared/shared.module';
+import { RoutingModule } from '../routing.module';
+
+import { HomeComponent } from '../home/home.component';
+import { SearchService } from '../services/search.service';
+
+import { TweetComponent } from '../tweet/tweet.component';
+import { TweetService } from '../services/tweet.service';
+
+describe('Component: Tweet', () => {
+    let component: TweetComponent;
+    let fixture: ComponentFixture<TweetComponent>;
+  
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({  
+        imports: [ 
+            SharedModule,
+            RoutingModule
+          ],  
+          declarations: [ HomeComponent , TweetComponent],
+          providers: [
+            TweetService,
+            SearchService,
+            {provide: APP_BASE_HREF, useValue: '/'}
+          ]
+        })
+      .compileComponents();
+    }));
+  
+    beforeEach(() => {
+      fixture = TestBed.createComponent(TweetComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+  
+    it('should open', () => {
+      expect(component).toBeTruthy();
+    });
+
+  });
+  
